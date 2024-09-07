91ec1c4bb1addecc67091df25bd6463a89f3bf2e
Oleh <oleh.chumakov2009@gmail.com>
2024-09-07 11:44:01 +0300

    Second Comit

--- Leson 1.py
+++ Leson 1.py
@@ -1,2 +1,31 @@
-print('Hello Wrld')
-da
+
+class Student:
+    count_of_student = 0
+    def __init__(self, name, age = 15):
+        self.name = name
+        self.age = age
+        print('Hi!')
+        Student.count_of_student += 1
+
+    def __str__(self):
+        print(f'Я {self.name}, мені {self.age} років')
+
+    def __str__(self):
+        print(f'Я {self.name},і я пішов ')
+        Student.count_of_student -= 1
+
+
+    def grow(self):
+
+print(Student.count_of_student)
+
+Anton = Student('Anton')
+print(Anton.age)
+Kiril = Student(name = 'Kiril', age = 17)
+print(Kiril.age)
+
+print(Anton.count_of_student)
+print(Student.count_of_student)
+
+Anton.info()
+Kiril.info()
